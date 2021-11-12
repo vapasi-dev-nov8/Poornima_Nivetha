8b4f304a521f3a6098ddebfa1e26d5f3cb05e531
nivetha <nivetha.karthikeyan@gmail.com>
2021-11-12 16:33:25 +0530

    |Poornima,Nivetha|change to ParameterizedTest  for  Rectangle

--- src/test/java/com/geometryshapes/RectangleTest.java
+++ src/test/java/com/geometryshapes/RectangleTest.java
@@ -1,16 +1,33 @@
 package com.geometryshapes;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class RectangleTest {
 
-    @Test
-    public void findAreaOfRectangle() {
-        Rectangle rectangle = new Rectangle(3,5);
-        assertEquals(15,rectangle.area());
+    @ParameterizedTest
+    @MethodSource("provideLengthBreadthAndArea")
+    public void findAreaOfRectangle(double length, double breadth, double expectedArea) {
+        Rectangle rectangle = new Rectangle(length,breadth);
+        double actualArea = rectangle.area();
+        assertEquals(expectedArea,actualArea);
+    }
+
+    private static Stream<Arguments> provideLengthBreadthAndArea() {
+        return Stream.of(
+                Arguments.of(2.0, 3.0, 6.0),
+                Arguments.of(2.0, 0.0, 0.0),
+                Arguments.of(0.0, 3.0, 0.0),
+                Arguments.of(0.0, 0.0, 0.0),
+                Arguments.of(3.0, 3.0, 9.0)
+        );
     }
 
     @Test
