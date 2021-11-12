f3d8be051ff45704b7b878cadb1bb2749158f8fe
nivetha <nivetha.karthikeyan@gmail.com>
2021-11-12 16:44:33 +0530

    |Nivetha|change ParameterizedTest for Rectangle perimeter

--- src/test/java/com/geometryshapes/RectangleTest.java
+++ src/test/java/com/geometryshapes/RectangleTest.java
@@ -1,6 +1,5 @@
 package com.geometryshapes;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -30,11 +29,22 @@ public class RectangleTest {
         );
     }
 
-    @Test
-    public void findPerimeterOfRectangle(){
-        Rectangle rectangle = new Rectangle(3,5);
-        double expectedResult = 2 * (3 + 5);
-        assertEquals(expectedResult,rectangle.perimeter());
+    @ParameterizedTest
+    @MethodSource("provideLengthBreadthAndPerimeter")
+    public void findPerimeterOfRectangle(double length, double breadth, double expectedPerimeter){
+        Rectangle rectangle = new Rectangle(length,breadth);
+        double actualPerimeter = rectangle.perimeter();
+        assertEquals(expectedPerimeter,actualPerimeter);
 
+    }
+
+    private static Stream<Arguments> provideLengthBreadthAndPerimeter() {
+        return Stream.of(
+                Arguments.of(2.0, 3.0, 10.0),
+                Arguments.of(2.0, 0.0, 4.0),
+                Arguments.of(0.0, 3.0, 6.0),
+                Arguments.of(0.0, 0.0, 0.0),
+                Arguments.of(3.0, 3.0, 12.0)
+        );
     }
 }
